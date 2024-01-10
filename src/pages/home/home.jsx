bee76d259bb82387707c64f9b8250c636645ecfa
shubham282raj <shubham282.me@gmail.com>
2024-01-10 19:00:31 +0530

    Fixed navbar padding, added blur in navbar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -13,28 +13,38 @@ const Home = () => {
     if (navbarPadding) navbarPadding.style.height = 0;
     const footerPadding = document.getElementById("universalFooterPadding");
     if (navbarPadding) footerPadding.style.height = 0;
-
+    const navbar = document.getElementById("navbar");
+    if (navbar) {
+      navbar.style.background = "transparent";
+      navbar.style.backdropFilter = "none";
+    }
+    window.scrollTo({ top: "0px" });
     return () => {
       // Reset the values when component is unmounted or navigated away from the homepage
       if (navbarPadding) navbarPadding.style.height = ""; // Reset to default value
       if (footerPadding) footerPadding.style.height = ""; // Reset to default value
+      if (navbar) {
+      navbar.style.background = "";
+      navbar.style.backdropFilter = "";
+    }
+      window.scrollTo({ top: "0px" });
     };
   }, []);
-  
+
   return (
     <div
       className="hostelHomePage"
       ref={homePage}
-      onWheel={(event)=>{
+      onWheel={(event) => {
         event.preventDefault();
-        if(event.deltaY > 0){
+        if (event.deltaY > 0) {
           homePage.current.scroll({
             top: homePage.current.scrollTop + homePage.current.clientHeight,
-          })
-        }else{
+          });
+        } else {
           homePage.current.scroll({
             top: homePage.current.scrollTop - homePage.current.clientHeight,
-          })
+          });
         }
       }}
     >

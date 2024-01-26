4f507f7c8914fbce7c9023efe6c7aa45d2089ae4
shubham282raj <shubham282.me@gmail.com>
2024-01-26 12:57:52 +0530

    Removed all changes in homePage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,44 +1,10 @@
-import { Slide1, Slide2 } from "./slides/slides";
 import "./home.css";
-import { useEffect, useRef } from "react";
-import { useHomeCSS } from "./components/useHomeCSS";
 
 const Home = () => {
-  const hostelHeading = document.getElementById("navbarHostelName");
-  if (hostelHeading) {
-    hostelHeading.textContent = "Hostel 5";
-  }
-  const homePage = useRef(null);
-  useHomeCSS();
-
-  let lastWheelEvent = Date.now();
-  useEffect(()=>{
-    const handleWheel = (event) => {
-      event.preventDefault();
-      if(Date.now() - lastWheelEvent < 300){
-        return;
-      }
-      lastWheelEvent = Date.now();
-      if (event.deltaY > 0) {
-        homePage.current.scroll({
-          top: homePage.current.scrollTop + homePage.current.clientHeight,
-        });
-      } else {
-        homePage.current.scroll({
-          top: homePage.current.scrollTop - homePage.current.clientHeight,
-        });
-      }
-    }
-    homePage.current.addEventListener("wheel", handleWheel)
-  });
 
   return (
-    <div
-      className="hostelHomePage"
-      ref={homePage}
-    >
-      <Slide1 />
-      <Slide2 />
+    <div>
+      <h1>Home</h1>
     </div>
   );
 };

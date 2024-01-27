1ec3383a524d4aaddfa0a7a441052f4e837ac887
shubham282raj <shubham282.me@gmail.com>
2024-01-27 21:34:11 +0530

    Load Lazy to All Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
-import Home from "./pages/home/home";
-import Tech from "./pages/tech/tech";
-import Cult from "./pages/cult/cult";
-import Sports from "./pages/sports/sports";
-import Mess from "./pages/mess/mess";
-import Council from "./pages/council/council";
-import LoginCouncil from "./pages/council/login/login";
-import CreatePost from "./pages/council/createPost/createPost";
-import DeletePost from "./pages/council/deletePost/deletePost";
-import EditPost from "./pages/council/editPost/editPost";
+import { LoadingCircleAnimation } from "./components/loadingCircleAnimation/loadingCircleAnimation";
+const Home = lazy(() => import("./pages/home/home"));
+const Tech = lazy(() => import("./pages/tech/tech"));
+const Cult = lazy(() => import("./pages/cult/cult"));
+const Sports = lazy(() => import("./pages/sports/sports"));
+const Mess = lazy(() => import("./pages/mess/mess"));
+const Council = lazy(() => import("./pages/council/council"));
+const LoginCouncil = lazy(() => import("./pages/council/login/login"));
+const CreatePost = lazy(() => import("./pages/council/createPost/createPost"));
+const DeletePost = lazy(() => import("./pages/council/deletePost/deletePost"));
+const EditPost = lazy(() => import("./pages/council/editPost/editPost"));
+// import Home from "./pages/home/home";
+// import Tech from "./pages/tech/tech";
+// import Cult from "./pages/cult/cult";
+// import Sports from "./pages/sports/sports";
+// import Mess from "./pages/mess/mess";
+// import Council from "./pages/council/council";
+// import LoginCouncil from "./pages/council/login/login";
+// import CreatePost from "./pages/council/createPost/createPost";
+// import DeletePost from "./pages/council/deletePost/deletePost";
+// import EditPost from "./pages/council/editPost/editPost";
 
 function App() {
   return (
@@ -18,24 +30,26 @@ function App() {
       <Router>
         <Navbar />
         <div id="universalNavbarPadding"></div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tech" element={<Tech />} />
-          <Route path="/cult" element={<Cult />} />
-          <Route path="/sports" element={<Sports />} />
-          <Route path="/mess" element={<Mess />} />
-          <Route path="/council">
-            <Route path="" element={<Council />} />
-            <Route path="login" element={<LoginCouncil />} />
-            <Route path="createPost" element={<CreatePost />} />
-            <Route path="deletePost" element={<DeletePost />} />
-            <Route path="editPost" element={<EditPost />} />
-            <Route
-              path="editPost/:division/:postType/:id"
-              Component={EditPost}
-            />
-          </Route>
-        </Routes>
+        <Suspense fallback={<LoadingCircleAnimation/>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tech" element={<Tech />} />
+            <Route path="/cult" element={<Cult />} />
+            <Route path="/sports" element={<Sports />} />
+            <Route path="/mess" element={<Mess />} />
+            <Route path="/council">
+              <Route path="" element={<Council />} />
+              <Route path="login" element={<LoginCouncil />} />
+              <Route path="createPost" element={<CreatePost />} />
+              <Route path="deletePost" element={<DeletePost />} />
+              <Route path="editPost" element={<EditPost />} />
+              <Route
+                path="editPost/:division/:postType/:id"
+                Component={EditPost}
+              />
+            </Route>
+          </Routes>
+        </Suspense>
       </Router>
       <div id="universalFooterPadding"></div>
     </div>
---
+++ src/components/loadingCircleAnimation/loadingCircleAnimation.jsx
@@ -0,0 +1,8 @@
+import "./loadingCircleAnimation.css"
+
+export const LoadingCircleAnimation = () =>{
+    return <div className="loadingCircleAnimation">
+        <div className="outerTriangle"></div>
+        <div className="innerTriangle"></div>
+    </div>
+}

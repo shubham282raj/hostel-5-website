aa1f4c90eaa0212f40816173813629202a5849a5
shubham282raj <shubham282.me@gmail.com>
2024-01-31 16:10:28 +0530

    Added Firebase Once on HomePage for Fast Loading

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,8 +2,12 @@ import "./home.css";
 import { LandingPage } from "./components/landingPage/landingPage";
 import { AboutUSPage } from "./components/aboutUsPage/aboutUsPage";
 import { OurMissionPage } from "./components/ourMissionPage/ourMissionPage";
+import { useLoadContent } from "../../components/loadPost/useLoadContent";
 
 const Home = () => {
+
+  const [loadFirebaseOnce] = useLoadContent("home", 1);
+
   const hostelHeading = document.getElementById("navbarHostelName");
   if (hostelHeading) {
     hostelHeading.textContent = "Hostel 5";
